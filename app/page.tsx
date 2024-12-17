bfb0c2831662f016744845bc34f0c70d39be644d
takahiro-okada <okahanako09875@gmail.com>
2024-12-18 06:42:20 +0900

    ⚡️ Use next/image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -7,7 +8,13 @@ export default function Home() {
         <h1 className={styles.top}></h1>
         <p className={styles.title}>テクノロジーの力で世界を変える</p>
       </div>
-      <img className={styles.bgimg} src="/img-mv.jpg" alt="" />
+      <Image
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        width={4000}
+        height={1200}
+        alt=""
+      />
     </section>
   );
 }
