ae219e6f4f2c94dbeb2703addafd11b5caa0742c
takahiro-okada <okahanako09875@gmail.com>
2024-12-25 06:36:13 +0900

    3章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,69 @@
+import ButtonLink from "./_components/ButtonLink";
 import styles from "./page.module.css";
 import Image from "next/image";
+import { News } from "@/app/_libs/microcms";
+import NewsList from "./_components/NewsList";
+
+const data: {
+  contents: News[];
+} = {
+  contents: [
+    {
+      id: "1",
+      title: "記事タイトル",
+      category: {
+        name: "カテゴリ名",
+      },
+      publishedAt: "2021-01-01T00:00:00Z",
+      createdAt: "2021-01-01T00:00:00Z",
+    },
+    {
+      id: "2",
+      title: "記事タイトル",
+      category: {
+        name: "カテゴリ名",
+      },
+      publishedAt: "2021-01-01T00:00:00Z",
+      createdAt: "2021-01-01T00:00:00Z",
+    },
+    {
+      id: "3",
+      title: "記事タイトル",
+      category: {
+        name: "カテゴリ名",
+      },
+      publishedAt: "2021-01-01T00:00:00Z",
+      createdAt: "2021-01-01T00:00:00Z",
+    },
+  ],
+};
 
 export default function Home() {
+  const sliceData = data.contents.slice(0, 2);
+
   return (
-    <section>
-      <div>
-        <h1 className={styles.top}></h1>
-        <p className={styles.title}>テクノロジーの力で世界を変える</p>
-      </div>
-      <Image
-        className={styles.bgimg}
-        src="/img-mv.jpg"
-        width={4000}
-        height={1200}
-        alt=""
-      />
-    </section>
+    <>
+      <section>
+        <div>
+          <h1 className={styles.top}></h1>
+          <p className={styles.title}>テクノロジーの力で世界を変える</p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          width={4000}
+          height={1200}
+          alt=""
+        />
+      </section>
+
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={sliceData} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっと見る</ButtonLink>
+        </div>
+      </section>
+    </>
   );
 }
