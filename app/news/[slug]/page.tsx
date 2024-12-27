b546da248891fdde39ece1af0360e6a6b5c5e4aa
takahiro-okada <okahanako09875@gmail.com>
2024-12-28 00:07:40 +0900

    サイト全体にrevalidateを設定する

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -13,8 +13,6 @@ type Props = {
   }>;
 };
 
-export const revalidate = 60;
-
 export default async function Page({ params, searchParams }: Props) {
   const resolvedParams = await params;
   const resolvedSearchParams = await searchParams;
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,8 +4,6 @@ import { NEWS_LIST_LIMIT } from "../_constants";
 import Pagination from "../_components/Pagination";
 import SearchField from "../_components/SearchField";
 
-export const revalidate = 60;
-
 export default async function Home() {
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
